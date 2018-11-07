18c9f879b7c207dc9005035b97262074940abef3
hackervincent <vincent@boiardt.se>
2018-11-07 17:20:25 +0100

    Create visual-studio-code.sh

---
+++ visual-studio-code.sh
@@ -0,0 +1,10 @@
+# install extensions
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension wix.vscode-import-cost
+code --install-extension eamodio.gitlens
+code --install-extension editorconfig.editorconfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension pkief.material-icon-theme
+code --install-extension shinnn.stylelint
+code --install-extension sleistner.vscode-fileutils
