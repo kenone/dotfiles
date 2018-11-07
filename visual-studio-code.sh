e55aa3cf10abb37773fcc6ffb88f986b2d9b50b7
hackervincent <vincent@boiardt.se>
2018-11-07 17:26:03 +0100

    Update visual-studio-code.sh

--- visual-studio-code.sh
+++ visual-studio-code.sh
@@ -8,3 +8,7 @@ code --install-extension ms-vscode.sublime-keybindings
 code --install-extension pkief.material-icon-theme
 code --install-extension shinnn.stylelint
 code --install-extension sleistner.vscode-fileutils
+
+# copy settings
+cp settings.json $HOME/Library/Application Support/Code/User/settings.json
+cp javascript.json $HOME/Library/Application Support/Code/User/snippets/javascript.json
